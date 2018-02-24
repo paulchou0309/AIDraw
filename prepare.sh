40f96bb86820e77efa34bb7f2d7b5626ffe528f6
王宏斌 <2484652469@qq.com>
2018-02-24 23:02:16 +0800

    add how to prepare google colab for training environment

---
+++ prepare.sh
@@ -0,0 +1,24 @@
+!apt-get install -y -qq software-properties-common python-software-properties module-init-tools
+!add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
+!apt-get update -qq 2>&1 > /dev/null
+!apt-get -y install -qq google-drive-ocamlfuse fuse
+from google.colab import auth
+auth.authenticate_user()
+from oauth2client.client import GoogleCredentials
+creds = GoogleCredentials.get_application_default()
+import getpass
+!google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL
+vcode = getpass.getpass()
+!echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}
+
+!mkdir -p drive
+!google-drive-ocamlfuse drive
+!tar zxf drive/DL/png_data.tar.gz
+!pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp36-cp36m-linux_x86_64.whl
+!pip3 install torchvision
+
+!pip install Pillow==4.0.0
+!pip install PIL
+!pip install image
+
+!tar zxf drive/DL/png_data.tar.gz
